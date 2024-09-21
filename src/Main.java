1d35ce7b59615c7f0d6e6f44df3966dbd52b2e96
daniil <gerel@bk.ru>
2024-09-21 09:29:59 +0500

    Even Nums

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
         System.out.println(factorial(10));
+        printEvenNums();
     }
     public  static int factorial(int n){
         if (n<0) return -1;
         else if (n==0) return 1;
         else return n*factorial(n-1);
+    }
+    public static void printEvenNums(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <=100 ; i++) {
+            if(i%2==0) {
+                sb.append(i);
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb.substring(0, sb.length()-2));
     }
 }
