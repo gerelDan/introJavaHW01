fd1e99f56a691cba668f0ac7125abc2a4c509cb7
daniil <gerel@bk.ru>
2024-09-21 09:19:35 +0500

    factorial

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
         System.out.println(factorial(10));
     }
     public  static int factorial(int n){
-        if (n<=0) return -1;
-        else if (n==1) return 1;
+        if (n<0) return -1;
+        else if (n==0) return 1;
         else return n*factorial(n-1);
     }
 }
